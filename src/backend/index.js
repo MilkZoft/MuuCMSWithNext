8cf00f7e21ead7119ae1c139ae6ed70b7b42269b
Carlos Santana <carlos@milkzoft.com>
2018-09-18 13:36:42 -0700

    First commit

---
+++ src/backend/index.js
@@ -0,0 +1,38 @@
+// Dependencies
+import express from 'express';
+import next from 'next';
+import path from 'path';
+
+// Environment
+const dev = process.env.NODE_ENV !== 'production';
+
+// Next App
+const nextApp = next({
+  dev,
+  dir: path.resolve(`${__dirname}/../frontend`)
+});
+
+// Next Handle
+const nextHandle = nextApp.getRequestHandler();
+
+nextApp
+  .prepare()
+  .then(() => {
+    // Express App
+    const app = express();
+
+    // Sending traffic to Next
+    app.get('*', (req, res) => nextHandle(req, res));
+
+    app.listen(3000, err => {
+      if (err) {
+        throw err;
+      }
+
+      console.log('Running server on http://localhost:3000');
+    });
+  })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
---
+++ src/frontend/pages/index.js
@@ -0,0 +1,5 @@
+import './index.scss';
+
+const Index = () => <h1>MuuCMS</h1>;
+
+export default Index;
